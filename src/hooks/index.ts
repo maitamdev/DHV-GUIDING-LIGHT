98e9db77875d40127a478333995b79b952837647
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:33:05 +0700

    feat(hooks): add barrel export file for all hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,20 @@
+﻿// Barrel export file for all custom hooks
+
+export { useLocalStorage } from './useLocalStorage';
+export { useDebounce } from './useDebounce';
+export { useMediaQuery, useIsMobile, useIsTablet, useIsDesktop } from './useMediaQuery';
+export { useScrollPosition } from './useScrollPosition';
+export { useClickOutside } from './useClickOutside';
+export { usePageTitle } from './usePageTitle';
+export { useToggle } from './useToggle';
+export { useCopyToClipboard } from './useCopyToClipboard';
+export { useCountdown } from './useCountdown';
+export { useWindowSize } from './useWindowSize';
+export { useIntersectionObserver } from './useIntersectionObserver';
+export { usePagination } from './usePagination';
+export { useSearch } from './useSearch';
+export { useOnlineStatus } from './useOnlineStatus';
+export { useKeyPress, useHotkey } from './useKeyPress';
+export { usePreviousValue } from './usePreviousValue';
+export { useFormValidation } from './useFormValidation';
+export { useScrollToTop } from './useScrollToTop';
