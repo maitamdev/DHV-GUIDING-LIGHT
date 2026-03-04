e8a8bfa817c4bba934c0a1ea4402af2595bdafcf
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:32:49 +0700

    feat(constants): add barrel export file

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+﻿// Barrel export file for all constants
+
+export * from './routes';
+export * from './config';
+export * from './colors';
+export * from './animations';
+export * from './menu';
+export * from './seo';
+export * from './api';
