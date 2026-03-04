4dae8280c52ce611190bf4e436d2615eb3d6b231
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:33:05 +0700

    feat(hooks): add useScrollToTop hook

---
+++ src/hooks/useScrollToTop.ts
@@ -0,0 +1,12 @@
+﻿import { useCallback } from 'react';
+
+/**
+ * Scroll to top of page with smooth animation
+ */
+export function useScrollToTop() {
+  const scrollToTop = useCallback((behavior: ScrollBehavior = 'smooth') => {
+    window.scrollTo({ top: 0, behavior });
+  }, []);
+
+  return scrollToTop;
+}
