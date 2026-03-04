3f553e46837234976e0afc8d7bfb913dc7f5ce7b
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:45:26 +0700

    feat(data): add barrel export for all data modules

---
+++ src/data/index.ts
@@ -0,0 +1,9 @@
+﻿export { coursesData } from './courses';
+export { categories } from './categories';
+export { testimonials } from './testimonials';
+export { faqData } from './faqData';
+export { blogPosts } from './blogPosts';
+export { roadmapsData } from './roadmaps';
+export { platformStats } from './stats';
+export { socialLinks } from './socialLinks';
+export { instructors } from './mentors';
