d92afd52c77cc9a889387b48f8a5ea5f9e0f5c73
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:37:43 +0700

    feat(layout): add barrel export for layout components

---
+++ src/components/layout/index.ts
@@ -0,0 +1,5 @@
+﻿export { default as Container } from './Container';
+export { default as Section } from './Section';
+export { default as PageHeader } from './PageHeader';
+export { default as DashboardLayout } from './DashboardLayout';
+export { default as AuthLayout } from './AuthLayout';
