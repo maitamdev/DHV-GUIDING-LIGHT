2ddedb1762e722c4ffcd0a8a8acbfa1ebb1d1d64
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:33:05 +0700

    feat(hooks): add usePreviousValue hook for state comparison

---
+++ src/hooks/usePreviousValue.ts
@@ -0,0 +1,14 @@
+﻿import { useRef, useEffect } from 'react';
+
+/**
+ * Get the previous value of a state or prop
+ */
+export function usePreviousValue<T>(value: T): T | undefined {
+  const ref = useRef<T | undefined>(undefined);
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
