454e743dbbfe3ae71111f4040d70542764c63f86
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:45:26 +0700

    feat(data): add social media links data

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,8 @@
+﻿import { FaFacebook, FaGithub, FaLinkedin, FaYoutube, FaTwitter } from 'react-icons/fa';
+export const socialLinks = [
+  { name: 'Facebook', url: 'https://facebook.com/dhvguidinglight', icon: FaFacebook, color: '#1877F2' },
+  { name: 'GitHub', url: 'https://github.com/maitamdev/DHV-GUIDING-LIGHT', icon: FaGithub, color: '#333' },
+  { name: 'LinkedIn', url: 'https://linkedin.com/company/dhvguidinglight', icon: FaLinkedin, color: '#0A66C2' },
+  { name: 'YouTube', url: 'https://youtube.com/@dhvguidinglight', icon: FaYoutube, color: '#FF0000' },
+  { name: 'Twitter', url: 'https://twitter.com/dhvguidinglight', icon: FaTwitter, color: '#1DA1F2' },
+];
