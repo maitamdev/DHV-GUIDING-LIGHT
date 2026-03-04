f3f1602cfb770060bdcecee03a3e74de076f895d
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:32:50 +0700

    feat(utils): add className merge utility

---
+++ src/utils/cn.ts
@@ -0,0 +1,20 @@
+﻿// ClassName merge utility for conditional class composition
+
+type ClassValue = string | number | boolean | undefined | null | ClassValue[];
+
+/**
+ * Merge class names conditionally - lightweight alternative to clsx
+ */
+export function cn(...classes: ClassValue[]): string {
+  return classes.flat(Infinity).filter(Boolean).join(' ');
+}
+
+/**
+ * Create conditional class string from object
+ */
+export function classMap(classes: Record<string, boolean | undefined>): string {
+  return Object.entries(classes)
+    .filter(([, condition]) => condition)
+    .map(([className]) => className)
+    .join(' ');
+}
