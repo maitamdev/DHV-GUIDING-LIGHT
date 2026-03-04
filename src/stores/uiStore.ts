66b2400817aa65865f1bfe18b370faa1745b2bb9
maitamgk <maitamit062005@gmail.com>
2026-03-05 01:37:45 +0700

    feat(stores): add Zustand UI state store

---
+++ src/stores/uiStore.ts
@@ -0,0 +1,15 @@
+﻿import { create } from 'zustand';
+
+interface UIStore {
+  isSidebarOpen: boolean; isSearchOpen: boolean; isNotificationOpen: boolean;
+  toggleSidebar: () => void; toggleSearch: () => void; toggleNotification: () => void;
+  closeAll: () => void;
+}
+
+export const useUIStore = create<UIStore>((set) => ({
+  isSidebarOpen: false, isSearchOpen: false, isNotificationOpen: false,
+  toggleSidebar: () => set(s => ({ isSidebarOpen: !s.isSidebarOpen, isSearchOpen: false, isNotificationOpen: false })),
+  toggleSearch: () => set(s => ({ isSearchOpen: !s.isSearchOpen, isSidebarOpen: false, isNotificationOpen: false })),
+  toggleNotification: () => set(s => ({ isNotificationOpen: !s.isNotificationOpen, isSidebarOpen: false, isSearchOpen: false })),
+  closeAll: () => set({ isSidebarOpen: false, isSearchOpen: false, isNotificationOpen: false }),
+}));
