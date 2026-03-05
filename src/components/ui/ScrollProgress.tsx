6720d58bfab20ae7b9d49ddc38a95b827460214d
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:47:04 +0700

    feat(ui): add scroll progress indicator

---
+++ src/components/ui/ScrollProgress.tsx
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+import { motion, useScroll } from 'framer-motion';
+const ScrollProgress: React.FC = () => {
+  const { scrollYProgress } = useScroll();
+  return <motion.div className='fixed top-0 left-0 right-0 h-1 bg-gradient-to-r from-blue-600 to-indigo-600 origin-left z-[100]' style={{ scaleX: scrollYProgress }} />;
+};
+export default ScrollProgress;
