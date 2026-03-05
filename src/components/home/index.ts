1c1a159b6cec56b32bf1f2fb624a2c62d0028908
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:58:14 +0700

    refactor(home): add barrel export for home components

---
+++ src/components/home/index.ts
@@ -0,0 +1,4 @@
+﻿export { default as HeroSection } from './HeroSection';
+export { default as FeaturesSection } from './FeaturesSection';
+export { default as StatsSection } from './StatsSection';
+export { default as CTASection } from './CTASection';
