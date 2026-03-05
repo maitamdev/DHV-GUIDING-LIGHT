b5ccf0ba38be5b28c6bfffb6613cfe665a3127fa
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:47:04 +0700

    feat(ui): add GradientText component

---
+++ src/components/ui/GradientText.tsx
@@ -0,0 +1,6 @@
+﻿import React from 'react';
+interface Props { children: React.ReactNode; from?: string; to?: string; className?: string; as?: keyof JSX.IntrinsicElements; }
+const GradientText: React.FC<Props> = ({ children, from = 'from-blue-600', to = 'to-indigo-600', className = '', as: Tag = 'span' }) => (
+  <Tag className={'bg-gradient-to-r bg-clip-text text-transparent ' + from + ' ' + to + ' ' + className}>{children}</Tag>
+);
+export default GradientText;
