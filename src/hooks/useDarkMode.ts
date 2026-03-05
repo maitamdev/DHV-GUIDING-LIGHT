79a92109c3d247ca2e6d6c30678b64215ace2847
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:46:58 +0700

    feat(hooks): add useDarkMode hook wrapper

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,6 @@
+﻿import { useDarkMode as useDarkModeContext } from '../context/DarkModeContext';
+export { useDarkModeContext as useDarkMode };
+export function useDarkClass(lightClass: string, darkClass: string): string {
+  const { isDarkMode } = useDarkModeContext();
+  return isDarkMode ? darkClass : lightClass;
+}
