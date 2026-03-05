87972670e0be0147d88bd53cff2a10f58c1a9bd2
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:56:44 +0700

    refactor(dashboard): add barrel export for dashboard components

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,6 @@
+﻿export { default as WelcomeHeader } from './WelcomeHeader';
+export { default as QuickActions } from './QuickActions';
+export { default as LearningProgress } from './LearningProgress';
+export { default as RecentActivity } from './RecentActivity';
+export { default as UpcomingDeadlines } from './UpcomingDeadlines';
+export { default as DashboardStats } from './DashboardStats';
