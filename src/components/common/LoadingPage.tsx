5ffd68dd5511a495aeb4b77eebbdfe66f4aa721c
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:47:06 +0700

    feat(components): add LoadingPage component

---
+++ src/components/common/LoadingPage.tsx
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+const LoadingPage: React.FC = () => (
+  <div className='min-h-screen flex items-center justify-center bg-gray-50'>
+    <div className='text-center'>
+      <div className='w-16 h-16 border-4 border-blue-200 border-t-blue-600 rounded-full animate-spin mx-auto mb-4' />
+      <p className='text-gray-500 font-medium'>Loading...</p>
+    </div>
+  </div>
+);
+export default LoadingPage;
