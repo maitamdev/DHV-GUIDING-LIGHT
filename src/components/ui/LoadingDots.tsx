dd3578afe828f5f0f068d014162e74a64e059335
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:47:05 +0700

    feat(ui): add LoadingDots component

---
+++ src/components/ui/LoadingDots.tsx
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+const LoadingDots: React.FC<{ className?: string }> = ({ className = '' }) => (
+  <span className={'inline-flex gap-1 ' + className}>
+    {[0, 1, 2].map(i => (<span key={i} className='w-2 h-2 bg-blue-600 rounded-full animate-bounce' style={{ animationDelay: i * 0.15 + 's' }} />))}
+  </span>
+);
+export default LoadingDots;
