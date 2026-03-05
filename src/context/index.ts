8768f5a11ba901f16513ee7aeda486b6de95162f
maitamgk <maitamit062005@gmail.com>
2026-03-05 12:00:34 +0700

    refactor(context): update barrel export with all contexts

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,2 +1,6 @@
 ﻿export { AuthProvider, useAuth } from './AuthContext';
 export { DarkModeProvider, useDarkMode } from './DarkModeContext';
+export { ToastProvider, useToast } from './ToastContext';
+export { ModalProvider, useModal } from './ModalContext';
+export { SidebarProvider, useSidebar } from './SidebarContext';
+export { SearchProvider, useSearch as useGlobalSearch } from './SearchContext';
