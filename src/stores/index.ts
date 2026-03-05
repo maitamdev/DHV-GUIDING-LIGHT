7c3a4b092af74fb04ec63aad9518bfcc61856d99
maitamgk <maitamit062005@gmail.com>
2026-03-05 12:04:15 +0700

    refactor(stores): update barrel export

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,3 +1,6 @@
 ﻿export { useCartStore } from './cartStore';
 export { useUIStore } from './uiStore';
 export { useNotificationStore } from './notificationStore';
+export { useSearchStore } from './searchStore';
+export { useSettingsStore } from './settingsStore';
+export { useCourseStore } from './courseStore';
