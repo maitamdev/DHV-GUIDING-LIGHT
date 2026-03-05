07f9188d83c0dc881b8ffce78b4e461c0f0e261a
maitamgk <maitamit062005@gmail.com>
2026-03-05 11:58:13 +0700

    refactor(course): add barrel export for course components

---
+++ src/components/course/index.ts
@@ -0,0 +1,5 @@
+﻿export { default as CourseHeader } from './CourseHeader';
+export { default as CourseSyllabus } from './CourseSyllabus';
+export { default as CourseReviews } from './CourseReviews';
+export { default as CourseInstructor } from './CourseInstructor';
+export { default as CoursePricing } from './CoursePricing';
