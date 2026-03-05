b9c56381cb24948d6e61c64c509b74180b63bce5
maitamgk <maitamit062005@gmail.com>
2026-03-05 12:08:02 +0700

    feat(utils): add debounce and throttle functions

---
+++ src/utils/debounce.ts
@@ -0,0 +1,19 @@
+﻿export function debounce<T extends (...args: unknown[]) => unknown>(fn: T, delay: number): T & { cancel: () => void } {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  const debounced = ((...args: unknown[]) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  }) as T & { cancel: () => void };
+  debounced.cancel = () => clearTimeout(timeoutId);
+  return debounced;
+}
+export function throttle<T extends (...args: unknown[]) => unknown>(fn: T, limit: number): T {
+  let inThrottle = false;
+  return ((...args: unknown[]) => {
+    if (!inThrottle) {
+      fn(...args);
+      inThrottle = true;
+      setTimeout(() => { inThrottle = false; }, limit);
+    }
+  }) as T;
+}
