d2f5c8e866d356829c2eb0afb33a9b3200732c08
maitamgk <maitamit062005@gmail.com>
2026-03-05 12:05:25 +0700

    test(utils): add colorUtils tests

---
+++ src/__tests__/utils/colorUtils.test.ts
@@ -0,0 +1,6 @@
+﻿import { describe, it, expect } from 'vitest';
+import { hexToRgb, rgbToHex, lighten, darken } from '../../utils/colorUtils';
+describe('colorUtils', () => {
+  describe('hexToRgb', () => { it('converts hex to rgb', () => { expect(hexToRgb('#ff0000')).toEqual({ r: 255, g: 0, b: 0 }); }); });
+  describe('rgbToHex', () => { it('converts rgb to hex', () => { expect(rgbToHex(255, 0, 0)).toBe('#ff0000'); }); });
+});
