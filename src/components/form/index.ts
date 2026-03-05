6e499b21744f5d8c1c22fb8f236fe0a7346d3476
maitamgk <maitamit062005@gmail.com>
2026-03-05 12:06:45 +0700

    feat(form): add barrel export for form components

---
+++ src/components/form/index.ts
@@ -0,0 +1,8 @@
+﻿export { default as FormGroup } from './FormGroup';
+export { default as TextInput } from './TextInput';
+export { default as TextArea } from './TextArea';
+export { default as Select } from './Select';
+export { default as Checkbox } from './Checkbox';
+export { default as RadioGroup } from './RadioGroup';
+export { default as DatePicker } from './DatePicker';
+export { default as RangeSlider } from './RangeSlider';
